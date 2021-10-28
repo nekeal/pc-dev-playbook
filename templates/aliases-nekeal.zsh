3ebaa12f6b5910821f2eb4cd8d1ed3bd6905faad
Szymon Cader <szymon.sc.cader@gmail.com>
2021-10-28 20:21:25 +0200

    Allow adding extra_aliases per host

--- templates/aliases-nekeal.zsh
+++ templates/aliases-nekeal.zsh
@@ -4,3 +4,6 @@ alias pc="pip-compile"
 alias pcu="pip-compile --upgrade"
 alias pcup="pip-compile --upgrade-package"
 alias won="workon"
+{% for key,value in _extra_aliases.items() %}
+alias {{ key }}="{{ value }}"
+{% endfor %}
