87ee1007424168191bff8742c5a405a03de053be
Szymon Cader <szymon.sc.cader@gmail.com>
2022-12-07 12:41:46 +0100

    Update envvars and aliases

--- templates/aliases-nekeal.zsh
+++ templates/aliases-nekeal.zsh
@@ -4,6 +4,11 @@ alias pc="pip-compile"
 alias pcu="pip-compile --upgrade"
 alias pcup="pip-compile --upgrade-package"
 alias won="workon"
+alias ghpr="gh pr"
+alias ghprc="ghpr create"
+alias ghmru="ghpr update"
+alias ghrv="gh repo view"
+alias ghrvw="gh repo view --web"
 {% for key,value in _extra_aliases.items() %}
 alias {{ key }}="{{ value }}"
 {% endfor %}
